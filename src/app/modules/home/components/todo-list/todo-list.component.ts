21e74312e8306b4cf3c23e3c4b76f7f90eea6fe1
ItsMatt1 <matedits03216@gmail.com>
2022-08-28 13:07:03 -0300

    emitting tasks

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  public taskList: Array<{ task: string, checked: boolean }> = [];
+  public taskList: Array<TaskList> = [
+    // { task: "Minha nova Task", checked: true },
+    // { task: "Minha nova Task 2", checked: false }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false })
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1)
+  }
+
+  public deleteAll() {
+    const confirm = window.confirm("Voce deseja realmente apagar todas as tasks?")
+
+    if (confirm)
+      this.taskList = []
+  }
 }
